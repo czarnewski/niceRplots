5183ed2e2198cde80b38e1b6548e893a240cb699
PauloCzarnewski <paulo.czarnewski@ki.se>
2020-03-20 10:08:55 -0300

    adds factor to colors

--- R/plotting_functions.R
+++ R/plotting_functions.R
@@ -7,44 +7,44 @@ require(spatstat)
 
 plot_feat <- function(x,red="umap",feat=NULL,label=NULL,assay="RNA",pch=16,bg=NA,cex=.3,dims=c(1,2),mins=0,add_graph=NULL,percent_connections=1,nbin=400,main=NULL,col=c("grey90","grey80","royalblue","navy","navy"),...){
   fn <- feat
-  
+
   if(feat %in% rownames(x@assays[[assay]]@data) ){
     feat <- x@assays[[assay]]@data[feat,]
-  } else if(feat %in% colnames(x@meta.data) ) { feat <- x@meta.data[,feat]  
+  } else if(feat %in% colnames(x@meta.data) ) { feat <- as.numeric(x@meta.data[,feat]  )
   } else { message("Feature or metadata not found!!") }
-  
+
   if(is.null(mins)){mins <- min(feat)}
-  
+
   feat <- (feat - mins)/ ( sort(feat,T)[ min(10,sum(feat!=0))  ] - mins)
   feat[feat > 1] <- 1
   o <- order(feat)
-  
+
   pal <- c( col[1],colorRampPalette(col[-1])(10))[round(feat*9)+1][o]
   #par(mar=c(1.5,1.5,1.5,1.5))
   options(warn=-1)
-  
+
   #creates plot
   plot( x@reductions[[red]]@cell.embeddings[o,dims] , type="n", xaxt="n",yaxt="n", main=ifelse( !is.null(main), main, paste0(assay,"_",fn)),...)
-  
+
   #adds underlying graph
   if(!is.null(add_graph) ){ if( add_graph %in% names(x@graphs)  ){
     add_graph(x,red=x@reductions[[red]]@cell.embeddings[o,dims],graph=add_graph,percent_connections=percent_connections) }else{message("Graph not found!")}}
-  
+
   #compute density and centroids
   if(!is.null(label) ){
     dens <- compute_density(x@reductions[[red]]@cell.embeddings[,dims],nbin=nbin)
-    centroids <-  sapply( unique(label) , 
-                          red=x@reductions[[red]]@cell.embeddings[,dims], 
-                          cl1=as.character(label), 
-                          function(jj,red,cl1) { apply( red[cl1==jj,],2, 
+    centroids <-  sapply( unique(label) ,
+                          red=x@reductions[[red]]@cell.embeddings[,dims],
+                          cl1=as.character(label),
+                          function(jj,red,cl1) { apply( red[cl1==jj,],2,
                              function(ii) {ndens <- dens[cl1==jj]
                              weighted.median(x = ii,w = 1/(1.1-ndens/(max(ndens))) )} )  })
     colnames(centroids) <- unique(label)}
-  
+
   #adds points
   points(x@reductions[[red]]@cell.embeddings[o,dims],pch=pch,cex=cex,bg=bg, col=paste0(pal) )
   options(warn=0)
-  
+
   #adds labels
   if(!is.null(label)){
     label <- factor(x@meta.data[[label]])
@@ -66,27 +66,27 @@ plot_meta <- function(x,red="umap",feat=NULL,pch=16,cex=.3,label=F,dims=c(1,2), 
   col <- col[feat]
   #par(mar=c(1.5,1.5,1.5,1.5))
   options(warn=-1)
-  
+
   #creates plot
   plot( x@reductions[[red]]@cell.embeddings[,dims], type="n", xaxt="n",yaxt="n",main=ifelse( !is.null(main), main, paste0(fn)),...)
 
   #adds underlying graph
   if(!is.null(add_graph) ){ if( add_graph %in% names(x@graphs)  ){
     add_graph(x,red=x@reductions[[red]]@cell.embeddings[,dims],graph=add_graph,percent_connections=percent_connections) }else{message("Graph not found!")}}
-  
+
   #compute density and centroids
   if(label | add_lines){
     dens <- compute_density(x@reductions[[red]]@cell.embeddings[,dims],nbin=nbin)
     centroids <-  sapply( as.character(levels(feat)) , reds=as.data.frame(x@reductions[[red]]@cell.embeddings[,dims]), cl1=feat, function(jj,reds,cl1) { apply( reds[cl1==jj,],2,function(ii) {ndens <- dens[cl1==jj];weighted.median(x = ii,w = 1/(1.1-ndens/(max(ndens))) )} )  })
     colnames(centroids) <- levels(feat)}
-  
+
   if(add_lines){
     add_centroid_lines(x@reductions[[red]]@cell.embeddings[,dims],feat,col,centroids)}
-  
+
   #adds points
   points(x@reductions[[red]]@cell.embeddings[,dims],pch=pch,cex=cex,bg=paste0(col,90), col=col )
   options(warn=0)
-  
+
   #adds labels
   if(label){
     points(centroids[1,],centroids[2,],col="#ffffff90",pch=16,cex=2)
@@ -134,8 +134,8 @@ add_centroid_lines <- function(red,feat,pal,centroids){
     c1 <- centroids[1,i]
     c2 <- centroids[2,i]
     apply(df,1,function(x){ lines(c(c1,x[1]),c(c2,x[2]),col=this_col,lwd=.2) })
-  } 
-  
+  }
+
 }
 
 
